ca69ed8effa0fd66eb58271d1d478906cb0b0c14
brian <brian@barnescocke.com>
2021-10-20 13:19:13 -0700

    started the docs and CLI parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
+use structopt::StructOpt;
+//use std::path::PathBuf;
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rtorrent-remote", about = "a transmission-remote-like client for rtorrent")]
 struct Cli {
-  pattern String,
-  path: std::path::PathBuf,
+  /// Help
+  #[structopt(short, long)]
+  help: bool,
+
+  /// Add Torrent
+  // Add torrent by filename or magnet URL
+  #[structopt(short = "a", long = "add")]
+  torrent: Option<String>,
+
+  /// Incomplete Directory
+  // Where to store new torrents until they are complete
+  #[structopt(short = "-c", long = "incomplete-dir")]
+  incompletedir: Option<String>,
+
+  /// No Incomplete Directory
+  // Don't store incomplete torrents in a different location
+  #[structopt(short = "C", long = "no-incomplete-dir")]
+  incompletedirbool: Option<bool>,
+
+  /// Debug
+  // Print Debug information
+  #[structopt(short, long)]
+  debug: bool,
+
+  //
+
 }
 fn main() {
-    println!("Hello, world!");
+  let cli = Cli::from_args();
+  println!("{:?}", cli);
 }
