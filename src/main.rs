2d0d661a271b40c3c701091009724951789415c0
brian <brian@barnescocke.com>
2021-10-20 14:35:44 -0700

    first day coding in forever, felt great

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "rtorrent-remote", about = "a transmission-remote-like client for rtorrent")]
 struct Cli {
-  /// Help
-  #[structopt(short, long)]
-  help: bool,
 
   /// Add Torrent
   // Add torrent by filename or magnet URL
@@ -15,7 +12,7 @@ struct Cli {
 
   /// Incomplete Directory
   // Where to store new torrents until they are complete
-  #[structopt(short = "-c", long = "incomplete-dir")]
+  #[structopt(short = "c", long = "incomplete-dir")]
   incompletedir: Option<String>,
 
   /// No Incomplete Directory
@@ -28,8 +25,78 @@ struct Cli {
   #[structopt(short, long)]
   debug: bool,
 
-  //
+  // Cache
+  // Set the maximum size of the sessions memory cache. Reset if rtorrent is restarted or closed.
+  #[structopt(short = "e", long = "cache")]
+  cachesize: Option<i32>,
 
+  /// Exit
+  // Tell rtorrent to close down
+  #[structopt(long = "exit")]
+  exitrtorrent: Option<bool>,
+
+  /// Files
+  // List the current torrent(s) files.
+  #[structopt(short = "f", long = "files")]
+  files: Option<bool>,
+
+  /// Info
+  // Show the current torrent(s) details
+  #[structopt(long = "info")]
+  infobool: Option<bool>,
+
+  /// Info Files
+  // List the current torrent(s) files.
+  #[structopt(long = "info-files")]
+  infofilebool: Option<bool>,
+
+  /// Info Peers
+  // List the current torrent(s)' peers.
+  #[structopt( long = "info-peers")]
+  infopeerbool: Option<bool>,
+
+  /// Info pieces
+  // List the current torrent(s)' pieces.
+  #[structopt(long = "info-pieces")]
+  infopieces: Option<bool>,
+
+  /// Info trackers
+  // List the current torrent(s) trackers.
+  #[structopt(long = "info-trackers")]
+  infotracker: Option<bool>,
+
+  /// Session info
+  // show the session's detail
+  #[structopt(long = "session-info")]
+  sessioninfo: Option<bool>,
+
+  /// Session stats
+  // Show the session's statistics
+  #[structopt(long = "session-stats")]
+  sessionstats: Option<bool>,
+
+  /// List Torrents
+  // List torrents
+  #[structopt(short = "l", long = "list")]
+  list: Option<bool>,
+
+  /// Labels
+  // set the current torrent(s)' labels
+  #[structopt(short = "L", long = "labels")]
+  labels: Option<Option<String>>,
+
+  /// Move
+  // Move Current torrent's data to a new folder
+  #[structopt(long = "move")]
+  movepath: Option<Option<String>>,
+
+  /// Find
+  // Tell Transmission where to find a torrent's data.
+  #[structopt(long = "find")]
+  findpath: Option<Option<String>>,
+
+  // Host
+  #[structopt(default)]
 }
 fn main() {
   let cli = Cli::from_args();
