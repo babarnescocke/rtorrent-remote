9d24524e83ab0483cc267ffba89e05bd3ef86c72
brian <brian@barnescocke.com>
2021-10-21 14:52:57 -0700

    added a few options

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct Cli {
   /// Add Torrent
   // Add torrent by filename or magnet URL
   #[structopt(short = "a", long = "add")]
-  torrent: Option<String>,
+  addtorrent: Option<String>,
 
   /// Incomplete Directory
   // Where to store new torrents until they are complete
@@ -96,7 +96,58 @@ struct Cli {
   findpath: Option<Option<String>>,
 
   // Host
-  #[structopt(default)]
+  // the URL of rtorrent
+  #[structopt(short, default_value = "http://localhost:8080/rpc2")] // https://github.com/rakshasa/rtorrent/wiki/RPC-Setup-XMLRPC gives this as the main 
+  rtorrenturl: String,
+
+  /// Tracker-Add
+  // Add tracker to current torrent(s)'
+  #[structopt(long = "tracker-add")]
+  tracker: Option<String>,       
+
+  /// Tracker-Remove
+  // Remove Tracker from current torrent(s)'
+  #[structopt(long = "tracker-remove")]
+  trackerid: Option<String>,
+
+  /// Start Torrent(s)
+  //Start the current torrents
+  #[structopt(short = "s", long = "start")]
+  start: Option<bool>,
+
+  /// Stop torrent(s)
+  // stop the current torrent(s)
+  #[structopt(short = "S", long = "stop")]
+  stop: Option<bool>,
+
+  /// Start paused
+  // Start added torrents paused
+  #[structopt(long = "start-paused")]
+  starttorpaused: Option<bool>,
+
+  /// Start added torrents unpaused
+  // start added torrents unpaused
+  #[structopt(long = "no-start-paused")]
+  starttorunpaused: Option<bool>,
+
+  /// torrent
+  // Set the current torrent(s) for use by subsequent options. The literal all will apply following requests to all torrents; the literal active will apply following requests to recently-active torrents; and specific torrents can be chosen by id or hash.  To set more than one current torrent, join their ids together in a list, such as "-t2,4,6-8" to operate on the torrents whose IDs are 2, 4, 6, 7, and 8.
+  #[structopt(short = "t", long = "torrent")]
+  torrent: Option<String>,
+
+  /// Enable UTP
+  #[structopt(long = "utp")]
+  utp: Option<bool>,
+
+  /// Disable UTP
+  #[structopt(long = "no-utp")]
+  noutp: Option<bool>,
+
+  /// Verify Current Torrent(s)
+  #[structopt(long = "verify", short = "V")]
+  verify: Option<bool>,
+
+
 }
 fn main() {
   let cli = Cli::from_args();
