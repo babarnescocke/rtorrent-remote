1b71ee1ef2f50661ae87fe3c1deeaee5212a16e5
brian <brian@barnescocke.com>
2021-11-01 11:24:10 -0700

    figuring out xmlrpc responses to arrays

--- src/xmlrpcbuilder.rs
+++ src/xmlrpcbuilder.rs
@@ -1,12 +1,17 @@
 use xmlrpc::Request;
 
 
-
 pub fn xmltester(rtorrenturl:&url::Url) {
-	let ls_request = Request::new("d.multicall2").arg("").arg("main").arg("d.bytes_done=").arg("d.up.rate=").arg("d.down.rate=").arg("d.state=").arg("d.name=");
+	let ls_request = Request::new("d.multicall2").arg("").arg("main").arg("d.bytes_done=").arg("d.size_bytes=").arg("d.up.rate=").arg("d.down.rate=").arg("d.state=").arg("d.name=");
 
-	let request_result = ls_request.call_url(rtorrenturl.as_str());
+	let request_result = ls_request.call_url(rtorrenturl.as_str()).unwrap();
+    
  // should print out ID, % Dne, Have, ETA, UP, DOWN, Ratio, Status Name
     println!("ID, % Done, Have, ETA, UP, Down, Ratio, Status, Name");
-	println!("Result: {:?}", request_result);
+    
 }
+
+// fn donepercent(donebytes:i64, bytes:i64) -> f64 {
+// 	return donebytes as f64/ bytes as f64;
+// }
+
