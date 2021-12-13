4d677094d089835101badcb6d0efcf03a9fae7bb
brian <brian@barnescocke.com>
2021-12-13 14:55:48 -0800

    a little housekeeping

--- src/istorrentvalidator.rs
+++
@@ -1,8 +0,0 @@
-use std::path::Path;
-
-// a simple function that validates if a given URL or Path - represented here as a string is OK. OK meaning if Path it is a syntactically valid and readable torrent file. If URL that its a readable 
-pub fn isTorrent(inString: &str) -> Result<(bool), ValidatorError> {
-	if Path::new(inString).isfile() { // will return true only if inString is readable and file
-	  if 
-	}
-}1
